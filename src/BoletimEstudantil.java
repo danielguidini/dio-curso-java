51ba554d294f9d544d258320a59fe024a1fe7eff
daniel-guidini <danielguidini2002@gmail.com>
2024-05-12 21:07:44 -0300

    terceiro commit

--- src/BoletimEstudantil.java
+++ src/BoletimEstudantil.java
@@ -1,2 +1,12 @@
 public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 5) {
+            System.out.println("Reprovado");
+        } else if (mediaFinal == 5) {
+            System.out.println("Aprovado rsrs");
+        } else {
+            System.out.println("Parabens");
+        }
+    }
 }
