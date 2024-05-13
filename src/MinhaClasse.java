d61fb7a994b0a043828a34184ca493947e316ee9
daniel-guidini <danielguidini2002@gmail.com>
2024-05-12 21:00:23 -0300

    segundo commit

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,3 +1,14 @@
-public class MinhaClasse {
+import org.jetbrains.annotations.NotNull;
 
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String meuNome = "Daniel";
+        String meuSobrenome = "Guidini";
+        final int anoNasc = 2002;
+        System.out.println(nomeCompleto(meuNome, meuSobrenome).concat(" ").concat(Integer.toString(anoNasc)));
+    }
+    @org.jetbrains.annotations.NotNull
+    public static String nomeCompleto(@NotNull String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
