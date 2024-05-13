e0d533c6b92eeb3e0cf15b8352842232df5e49b7
daniel-guidini <danielguidini2002@gmail.com>
2024-05-13 19:54:03 -0300

    inclusão da aula 2

---
+++ src/edu/daniel/aula2/Variaveis.java
@@ -0,0 +1,10 @@
+package edu.daniel.aula2;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 1412.00;
+        float salarioMinimoF = (float) salarioMinimo;
+        // int e double são mais usados.
+        final int ANO_ATUAL = 2024;
+    }
+}
